4e3bf757f6a3f5b62a43c6ddb0c302fe327b5712
BartOutOfTheBox <bart.verhoeven1@student.kuleuven.be>
2014-11-12 12:07:21 +0100

    dynamically loading of new versions

---
+++ js/version.js
@@ -0,0 +1,28 @@
+<!-- Hide from older browsers
+function getVersion() {
+	var xmlHttp = null;
+
+	xmlHttp = new XMLHttpRequest();
+	xmlHttp.open( "GET", "http://camambar.github.io/CheeseSqueeze/version", false );
+	xmlHttp.send( null );
+	vString = xmlHttp.responseText.split("\n")[1];
+	vString = vString.split(".").join("_");
+	return vString;
+}
+
+function getAndroidAPK() {
+	return "http://camambar.github.io/CheeseSqueeze/releases/android-debug-v" + version + ".apk";
+}
+
+function getWeb() {
+	return "http://camambar.github.io/CheeseSqueeze/game/web";
+}
+
+function getDesktopJAR() {
+	return "http://camambar.github.io/CheeseSqueeze/releases/CheeseSqueeze-v" + version + ".jar";
+}
+
+function getIOS() {
+	return "";
+}
+//stop hiding-->
